41c7173a84d805adab32af9c5babc70d186dcce5
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-04-19 13:08:53 +0900

    Updated CharacterDataXMLParser.

--- xopsmanipulator-properties/src/com/daxie/xops/properties/xml/CharacterDataXMLParser.java
+++ xopsmanipulator-properties/src/com/daxie/xops/properties/xml/CharacterDataXMLParser.java
@@ -78,105 +78,105 @@ public class CharacterDataXMLParser {
 			Element element=(Element)node;
 			String element_name=element.getNodeName();
 			
-			if(element_name.equals("character")) {
-				int character_id=-1;
-				CharacterData character_data=new CharacterData();
+			if(element_name.equals("character")==false)continue;
+			
+			int character_id=-1;
+			CharacterData character_data=new CharacterData();
+			
+			String strtemp;
+			strtemp=element.getAttribute("id");
+			try {
+				character_id=Integer.parseInt(strtemp);
+			}
+			catch(NumberFormatException e) {
+				logger.warn("Invalid format of number. index={} character.<id>",i);
+				continue;
+			}
+			
+			int itemp;
+			NodeList node_list_sub=element.getChildNodes();
+			for(int j=0;j<node_list_sub.getLength();j++) {
+				Node node_sub=node_list_sub.item(j);
+				if(node_sub.getNodeType()!=Node.ELEMENT_NODE)continue;
 				
-				String strtemp;
-				strtemp=element.getAttribute("id");
+				Element element_sub=(Element)node_sub;
+				String element_sub_name=element_sub.getNodeName();
+				
+				strtemp=element_sub.getTextContent();
 				try {
-					character_id=Integer.parseInt(strtemp);
+					itemp=Integer.parseInt(strtemp);
 				}
 				catch(NumberFormatException e) {
-					logger.warn("Invalid format of number. index={} character.<id>",i);
+					logger.warn("Invalid format of number. index={} character.{}",i,element_sub_name);
 					continue;
 				}
 				
-				int itemp;
-				NodeList node_list_sub=element.getChildNodes();
-				for(int j=0;j<node_list_sub.getLength();j++) {
-					Node node_sub=node_list_sub.item(j);
-					if(node_sub.getNodeType()!=Node.ELEMENT_NODE)continue;
-					
-					Element element_sub=(Element)node_sub;
-					String element_sub_name=element_sub.getNodeName();
-					
-					strtemp=element_sub.getTextContent();
-					try {
-						itemp=Integer.parseInt(strtemp);
+				//Texture
+				if(element_sub_name.equals("texture")) {
+					if(openxops_compatible_flag==true) {
+						CharacterTextureType texture_type;
+						texture_type=CharacterSpecifierConverter.GetXOPSTextureTypeFromOpenXOPSTextureID(itemp);
+						character_data.SetTextureType(texture_type);
 					}
-					catch(NumberFormatException e) {
-						logger.warn("Invalid format of number. index={} character.{}",i,element_sub_name);
-						continue;
-					}
-					
-					//Texture
-					if(element_sub_name.equals("texture")) {
-						if(openxops_compatible_flag==true) {
-							CharacterTextureType texture_type;
-							texture_type=CharacterSpecifierConverter.GetXOPSTextureTypeFromOpenXOPSTextureID(itemp);
-							character_data.SetTextureType(texture_type);
+					else {
+						CharacterTextureType[] values=CharacterTextureType.values();
+						if(!(0<=itemp&&itemp<values.length)) {
+							logger.warn("Specifier out of bounds. index={} character.texture",i);
+							character_data.SetTextureType(CharacterTextureType.SOLDIER_BLACK);
 						}
 						else {
-							CharacterTextureType[] values=CharacterTextureType.values();
-							if(!(0<=itemp&&itemp<values.length)) {
-								logger.warn("Specifier out of bounds. index={} character.texture",i);
-								character_data.SetTextureType(CharacterTextureType.SOLDIER_BLACK);
-							}
-							else {
-								character_data.SetTextureType(values[itemp]);
-							}
+							character_data.SetTextureType(values[itemp]);
 						}
-					}
-					//Model
-					else if(element_sub_name.equals("model")) {
-						CharacterModelType[] values=CharacterModelType.values();
-						if(!(0<=itemp&&itemp<values.length)) {
-							logger.warn("Specifier out of bounds. index={} character.model",i);
-							character_data.SetModelType(CharacterModelType.MALE);
-						}
-						else character_data.SetModelType(values[itemp]);
-					}
-					//HP
-					else if(element_sub_name.equals("hp")) {
-						character_data.SetHP(itemp);
-					}
-					//AI level
-					else if(element_sub_name.equals("AIlevel")) {
-						if(openxops_compatible_flag==true) {
-							CharacterAILevel ai_level=CharacterSpecifierConverter.GetXOPSAILevelFromOpenXOPSAILevel(itemp);
-							character_data.SetAILevel(ai_level);
-						}
-						else {
-							CharacterAILevel[] values=CharacterAILevel.values();
-							if(!(0<=itemp&&itemp<values.length)) {
-								logger.warn("Specifier out of bounds. index={} character.AIlevel",i);
-								character_data.SetAILevel(CharacterAILevel.C);
-							}
-							else character_data.SetAILevel(values[itemp]);
-						}
-					}
-					//Weapon A
-					else if(element_sub_name.equals("WeaponA")) {
-						character_data.SetWeaponID(0, itemp);
-					}
-					//Weapon B
-					else if(element_sub_name.equals("WeaponB")) {
-						character_data.SetWeaponID(1, itemp);
-					}
-					//Type
-					else if(element_sub_name.equals("type")) {
-						CharacterType[] values=CharacterType.values();
-						if(!(0<=itemp&&itemp<values.length)) {
-							logger.warn("Specifier out of bounds. index={} character.type",i);
-							character_data.SetType(CharacterType.HUMAN);
-						}
-						else character_data.SetType(values[itemp]);
 					}
 				}
-				
-				character_data_map.put(character_id, character_data);
+				//Model
+				else if(element_sub_name.equals("model")) {
+					CharacterModelType[] values=CharacterModelType.values();
+					if(!(0<=itemp&&itemp<values.length)) {
+						logger.warn("Specifier out of bounds. index={} character.model",i);
+						character_data.SetModelType(CharacterModelType.MALE);
+					}
+					else character_data.SetModelType(values[itemp]);
+				}
+				//HP
+				else if(element_sub_name.equals("hp")) {
+					character_data.SetHP(itemp);
+				}
+				//AI level
+				else if(element_sub_name.equals("AIlevel")) {
+					if(openxops_compatible_flag==true) {
+						CharacterAILevel ai_level=CharacterSpecifierConverter.GetXOPSAILevelFromOpenXOPSAILevel(itemp);
+						character_data.SetAILevel(ai_level);
+					}
+					else {
+						CharacterAILevel[] values=CharacterAILevel.values();
+						if(!(0<=itemp&&itemp<values.length)) {
+							logger.warn("Specifier out of bounds. index={} character.AIlevel",i);
+							character_data.SetAILevel(CharacterAILevel.C);
+						}
+						else character_data.SetAILevel(values[itemp]);
+					}
+				}
+				//Weapon A
+				else if(element_sub_name.equals("WeaponA")) {
+					character_data.SetWeaponID(0, itemp);
+				}
+				//Weapon B
+				else if(element_sub_name.equals("WeaponB")) {
+					character_data.SetWeaponID(1, itemp);
+				}
+				//Type
+				else if(element_sub_name.equals("type")) {
+					CharacterType[] values=CharacterType.values();
+					if(!(0<=itemp&&itemp<values.length)) {
+						logger.warn("Specifier out of bounds. index={} character.type",i);
+						character_data.SetType(CharacterType.HUMAN);
+					}
+					else character_data.SetType(values[itemp]);
+				}
 			}
+			
+			character_data_map.put(character_id, character_data);
 		}
 		
 		return 0;
