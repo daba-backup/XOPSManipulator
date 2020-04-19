677cc21f9943efc849305e54b1fed449510eb663
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-04-19 18:20:02 +0900

    Code refactoring in BD1Writer.

--- xopsmanipulator-bd1/src/com/daxie/xops/bd1/BD1Writer.java
+++ xopsmanipulator-bd1/src/com/daxie/xops/bd1/BD1Writer.java
@@ -1,9 +1,7 @@
 package com.daxie.xops.bd1;
 
-import java.io.BufferedOutputStream;
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -13,6 +11,7 @@ import org.slf4j.LoggerFactory;
 
 import com.daxie.basis.vector.Vector;
 import com.daxie.tool.ByteFunctions;
+import com.daxie.tool.FileFunctions;
 
 /**
  * Writes data to a BD1 file.
@@ -36,97 +35,117 @@ class BD1Writer {
 			return -1;
 		}
 		
-		try(DataOutputStream dos=new DataOutputStream(new BufferedOutputStream(new FileOutputStream(bd1_filename)))) {
-			//Texture filenames
-			Map<Integer, String> sorted_texture_filenames_map=new TreeMap<>(texture_filenames_map);
+		List<Byte> bin=new ArrayList<>();
+		
+		this.AddTextureFilenamesToBin(bin);
+		
+		//Number of blocks
+		int block_num=blocks.size();
+		this.AddUShortToBin(bin, block_num);
+		
+		//Block data
+		for(int i=0;i<blocks.size();i++) {
+			BD1Block block=blocks.get(i);
 			
-			int texture_count=0;
-			for(int texture_id:sorted_texture_filenames_map.keySet()) {
-				String texture_filename=sorted_texture_filenames_map.get(texture_id);
-				if(texture_filename.length()>30) {
-					texture_filename=texture_filename.substring(0, 30);
-				}
-				
-				byte[] texture_filename_buffer=new byte[31];
-				
-				int pos;
-				for(pos=0;pos<texture_filename.length();pos++) {
-					texture_filename_buffer[pos]=(byte)texture_filename.charAt(pos);
-				}
-				for(;pos<texture_filename_buffer.length;pos++) {
-					texture_filename_buffer[pos]=0;
-				}
-				
-				dos.write(texture_filename_buffer);
-				texture_count++;
+			Vector[] vertex_positions=block.GetVertexPositions();
+			float[] us=block.GetUs();
+			float[] vs=block.GetVs();
+			int[] texture_ids=block.GetTextureIDs();
+			
+			byte[] b;
+			
+			//Vertex positions
+			for(int j=0;j<8;j++) {
+				this.AddFloatToBin(bin, vertex_positions[j].GetX());
+			}
+			for(int j=0;j<8;j++) {
+				this.AddFloatToBin(bin, vertex_positions[j].GetY());
+			}
+			for(int j=0;j<8;j++) {
+				this.AddFloatToBin(bin, vertex_positions[j].GetZ());
 			}
 			
-			byte[] null_buffer=new byte[31];
-			for(int i=0;i<31;i++)null_buffer[i]=0;
-			for(int i=texture_count;i<10;i++) {
-				dos.write(null_buffer);
+			//UV coordinates
+			for(int j=0;j<24;j++) {
+				this.AddFloatToBin(bin, us[j]);
+			}
+			for(int j=0;j<24;j++) {
+				this.AddFloatToBin(bin, vs[j]);
 			}
 			
-			//Number of blocks
-			int block_num=blocks.size();
-			
-			byte[] block_num_buffer=ByteFunctions.ushort_to_byte_le(block_num);
-			dos.write(block_num_buffer);
-			
-			//Block data
-			for(int i=0;i<blocks.size();i++) {
-				BD1Block block=blocks.get(i);
-				
-				Vector[] vertex_positions=block.GetVertexPositions();
-				float[] us=block.GetUs();
-				float[] vs=block.GetVs();
-				int[] texture_ids=block.GetTextureIDs();
-				
-				byte[] b;
-				
-				//Vertex positions
-				for(int j=0;j<8;j++) {
-					b=ByteFunctions.float_to_byte_le(vertex_positions[j].GetX());
-					dos.write(b);
+			//Texture IDs
+			for(int j=0;j<6;j++) {
+				bin.add((byte)texture_ids[j]);
+				for(int k=0;k<3;k++) {
+					bin.add((byte)0);
 				}
-				for(int j=0;j<8;j++) {
-					b=ByteFunctions.float_to_byte_le(vertex_positions[j].GetY());
-					dos.write(b);
-				}
-				for(int j=0;j<8;j++) {
-					b=ByteFunctions.float_to_byte_le(vertex_positions[j].GetZ());
-					dos.write(b);
-				}
-				
-				//UV coordinates
-				for(int j=0;j<24;j++) {
-					b=ByteFunctions.float_to_byte_le(us[j]);
-					dos.write(b);
-				}
-				for(int j=0;j<24;j++) {
-					b=ByteFunctions.float_to_byte_le(vs[j]);
-					dos.write(b);
-				}
-				
-				//Texture IDs
-				for(int j=0;j<6;j++) {
-					dos.writeByte(texture_ids[j]);
-					for(int k=0;k<3;k++)dos.writeByte(0);
-				}
-				
-				//Enabled flag
-				if(block.GetEnabledFlag()==true)dos.writeByte(1);
-				else dos.writeByte(0);
-				for(int j=0;j<3;j++)dos.writeByte(0);
 			}
 			
-			dos.flush();
+			//Enabled flag
+			if(block.GetEnabledFlag()==true) {
+				bin.add((byte)1);
+			}
+			else{
+				bin.add((byte)0);
+			}
+			for(int j=0;j<3;j++) {
+				bin.add((byte)0);
+			}
+		}
+		
+		try {
+			FileFunctions.CreateBinFile(bd1_filename, bin);
 		}
 		catch(IOException e) {
 			logger.error("Error while writing.",e);
 			return -1;
 		}
-		
+			
 		return 0;
-	}	
+	}
+	private void AddTextureFilenamesToBin(List<Byte> bin) {
+		Map<Integer, String> sorted_texture_filenames_map=new TreeMap<>(texture_filenames_map);
+		
+		int texture_count=0;
+		for(int texture_id:sorted_texture_filenames_map.keySet()) {
+			String texture_filename=sorted_texture_filenames_map.get(texture_id);
+			if(texture_filename.length()>30) {
+				texture_filename=texture_filename.substring(0, 30);
+			}
+			
+			byte[] texture_filename_buffer=new byte[31];
+			
+			int pos;
+			for(pos=0;pos<texture_filename.length();pos++) {
+				texture_filename_buffer[pos]=(byte)texture_filename.charAt(pos);
+			}
+			for(;pos<texture_filename_buffer.length;pos++) {
+				texture_filename_buffer[pos]=0;
+			}
+			
+			for(int i=0;i<31;i++) {
+				bin.add(texture_filename_buffer[i]);
+			}
+			
+			texture_count++;
+		}
+		
+		for(int i=texture_count;i<10;i++) {
+			for(int j=0;j<31;j++) {
+				bin.add((byte)0);
+			}
+		}
+	}
+	private void AddUShortToBin(List<Byte> bin,int s) {
+		byte[] buffer=ByteFunctions.ushort_to_byte_le(s);
+		for(int i=0;i<2;i++) {
+			bin.add(buffer[i]);
+		}
+	}
+	private void AddFloatToBin(List<Byte> bin,float f) {
+		byte[] buffer=ByteFunctions.float_to_byte_le(f);
+		for(int i=0;i<4;i++) {
+			bin.add(buffer[i]);
+		}
+	}
 }
