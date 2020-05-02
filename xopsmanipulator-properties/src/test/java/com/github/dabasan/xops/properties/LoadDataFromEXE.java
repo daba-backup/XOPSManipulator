61e36143cc671767165ec88fb579bfea2623f1d5
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-05-03 00:18:56 +0900

    Added a test class.

---
+++ xopsmanipulator-properties/src/test/java/com/github/dabasan/xops/properties/LoadDataFromEXE.java
@@ -0,0 +1,33 @@
+package com.github.dabasan.xops.properties;
+
+import java.io.IOException;
+
+import com.github.dabasan.xops.properties.entity.character.CharacterData;
+import com.github.dabasan.xops.properties.entity.weapon.WeaponData;
+import com.github.dabasan.xops.properties.exe.XOPSExeManipulator;
+
+public class LoadDataFromEXE {
+	public static void main(String[] args) {
+		XOPSExeManipulator manipulator;
+		try {
+			manipulator=new XOPSExeManipulator("./TestData/xops0975t.exe");
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		WeaponData[] weapon_data=manipulator.GetWeaponDataArray();
+		CharacterData[] character_data=manipulator.GetCharacterDataArray();
+		
+		for(int i=0;i<weapon_data.length;i++) {
+			System.out.printf("[%d]\n", i);
+			System.out.println(weapon_data[i]);
+		}
+		System.out.println("====================");
+		for(int i=0;i<character_data.length;i++) {
+			System.out.printf("[%d]\n", i);
+			System.out.println(character_data[i]);
+		}
+	}
+}
